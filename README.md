609f363c9560d8c0ba1ba11ad1336bccac19843d
seonghwan7694 <seonghwan7694@gmail.com>
2022-12-18 09:49:00 +0900

    🐇  develop pygame (2)
    🛠 해야할 것
    - 초기화면, 게임화면 레이아웃 구성하기
    - 텍스트 박스 클래스 정의하기

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # rhythm-game
 pygame - making rhythm-game in 24 hours.
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+![Ubuntu](https://img.shields.io/badge/Ubuntu-E95420?style=for-the-badge&logo=ubuntu&logoColor=white)
+
+## user-guide
+
+### install & run
+```bash
+git clone https://github.com/seonghwan7694/rhythm-game.git
+cd rhytrhm-game
+poetry shell
+poetry run python main.py
+```
+
+### play
+
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import sys
 import pygame
 from pygame.locals import *
 
-pygame.init()
 FPS = 60
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -10,36 +9,61 @@ FramePerSec = pygame.time.Clock()
 GameDisplay = pygame.display.set_mode((500, 1440))
 GameDisplay.fill(white)
 pygame.display.set_caption("rhythm-game")
-music = pygame.mixer.Sound("SIIK & Alenn - Mess [NCS Release].mp3")
+
+
+class TextBox:
+  def __init__(self, screen, pos):
+    self.font = pygame.font.SysFont('Arial', 25)
+    self.screen = screen
+    self.pos = pos
+    pygame.display.update()
+
+  def addRect(self):
+    self.rect = pygame.draw.rect(self.screen, black, (175, 75, 200, 100), 2)
+    pygame.display.update()
+
+  def addText(self):
+    self.screen.blit(self.font.render('Hello!', True, (255, 0, 0)), (200, 100))
+    pygame.display.update()
 
 
 def main_page():
-  area = pygame.draw.rect(GameDisplay, black, [0, 0, 30, 30])
+  play_btn = pygame.draw.rect(GameDisplay, black, [0, 0, 30, 30])
+  quit_btn = pygame.draw.rect(GameDisplay, black, [50, 50, 30, 30])
   while True:
-    pygame.display.update()
+    pygame.display.flip()
     for event in pygame.event.get():
       if event.type == pygame.MOUSEBUTTONDOWN:
         if event.button == 1:
-          if area.collidepoint(event.pos):
-            print("클릭됨.")
+          if quit_btn.collidepoint(event.pos):
+            return 0
+          if play_btn.collidepoint(event.pos):
+            return 1
+      if event.type == pygame.QUIT:
+        return 0
+    FramePerSec.tick(FPS)
+
+
+def game_page():
+  pygame.mixer.init()
+  pygame.mixer.music.load("SIIK & Alenn - Mess [NCS Release].mp3")
+  pygame.mixer.music.play()
+  while True:
+    pygame.display.flip()
+    for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return None
     FramePerSec.tick(FPS)
 
 
 if __name__ == '__main__':
+  running = True
   pygame.init()
-  main_page()
-
-  pygame.quit()
-  sys.exit()
-
-"""
-while True:
-  pygame.display.update()
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
+  while running:
+    var = main_page()
+    if var == 0:
+      running = False
+    elif var == 1:
+      game_page()
       pygame.quit()
       sys.exit()
-  FramePerSec.tick(FPS)
-"""
